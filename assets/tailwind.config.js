9eaab71cf4d9da0180a392705b1c1ca94e7ad3e9
Ian Duggan <ian@ianduggan.net>
2023-06-24 22:57:11 -0700

    adjust core colors

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors.blue,
-        secondary: colors.pink,
+        primary: colors.emerald,
+        secondary: colors.orange,
         success: colors.green,
         danger: colors.red,
         warning: colors.yellow,
